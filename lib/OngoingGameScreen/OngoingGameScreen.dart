77bfb438c5420f9e20f35faca695982b9d00e58a
Lopez <hansel.lopez@windows.nagrastar.com>
2019-01-02 23:09:30 -0700

    update: Implement basic features of OngoingScreen

--- lib/OngoingGameScreen/OngoingGameScreen.dart
+++ lib/OngoingGameScreen/OngoingGameScreen.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/Shared/ActionBar.dart';
+import 'package:hello_world/Shared/BorderContainer.dart';
+import 'package:hello_world/Shared/Navigate.dart';
 import 'package:hello_world/Shared/SpyfallPage.dart';
+import 'package:hello_world/Shared/TopHeader.dart';
 
 class OngoingGameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String title = "7:39";
+    Navigate navigate = Navigate.of(context);
+
+    final List<MenuItem> menuItems = [
+      MenuItem("End Game", () => navigate.goToPage(Pages.PRE_GAME_SCREEN)),
+      MenuItem("Leave Game", () => navigate.goToPage(Pages.HOME_SCREEN)),
+    ];
+
     return SpyfallPage(
-      child: Text('Ongoing Game'),
+      child: Column(
+        children: <Widget>[
+          TopHeader(title),
+          BorderContainer(
+            child: ActionBar(menuItems),
+          ),
+        ],
+      ),
     );
   }
 }
