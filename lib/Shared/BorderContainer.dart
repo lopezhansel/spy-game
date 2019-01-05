197504bd5ae18c24c9fb3d6c8a860e6127fa27f0
Lopez <hansel.lopez@windows.nagrastar.com>
2019-01-05 14:48:07 -0700

    update: Change border color

--- lib/Shared/BorderContainer.dart
+++ lib/Shared/BorderContainer.dart
@@ -13,8 +13,8 @@ class BorderContainer extends StatelessWidget {
       padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
       decoration: const BoxDecoration(
         border: Border(
-          bottom: BorderSide(width: 0.5, color: Color(0xFFFF000000)),
-          top: BorderSide(width: 0.5, color: Color(0xFFFF000000)),
+          bottom: BorderSide(width: 0.5, color: Color(0xFFEEEEEE)),
+          top: BorderSide(width: 0.5, color: Color(0xFFEEEEEE)),
         ),
       ),
     );
