46822a2146016098088eafbdd5d88128c2e841ff
Lopez <hansel.lopez@windows.nagrastar.com>
2019-01-05 16:41:21 -0700

    update: Add support to disable top and bottom border

--- lib/Shared/BorderContainer.dart
+++ lib/Shared/BorderContainer.dart
@@ -2,19 +2,30 @@ import 'package:flutter/material.dart';
 
 class BorderContainer extends StatelessWidget {
   final Widget child;
+  final bool bottomBorder;
+  final bool topBorder;
 
-  BorderContainer({this.child});
+  BorderContainer(
+      {this.child, this.topBorder = true, this.bottomBorder = true});
+  BorderContainer.top(
+      {this.child, this.bottomBorder = false, this.topBorder = true});
+  BorderContainer.bottom(
+      {this.child, this.bottomBorder = true, this.topBorder = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment(0.0, 0.0),
+      alignment: const Alignment(0.0, 0.0),
       child: child,
-      padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-      decoration: const BoxDecoration(
+      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+      decoration: BoxDecoration(
         border: Border(
-          bottom: BorderSide(width: 0.5, color: Color(0xFFEEEEEE)),
-          top: BorderSide(width: 0.5, color: Color(0xFFEEEEEE)),
+          bottom: bottomBorder
+              ? BorderSide(width: 1.0, color: Color(0xFFEEEEEE))
+              : BorderSide.none,
+          top: topBorder
+              ? BorderSide(width: 1.0, color: Color(0xFFEEEEEE))
+              : BorderSide.none,
         ),
       ),
     );
