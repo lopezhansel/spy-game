9d9344142d671f6916e4a66c0add60769418c235
Lopez <hansel.lopez@windows.nagrastar.com>
2019-01-05 20:47:31 -0700

    update: Change style of font

--- lib/CreateGameSreen/PlayerList.dart
+++ lib/CreateGameSreen/PlayerList.dart
@@ -5,7 +5,15 @@ class PlayerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BorderContainer.top(
-      child: Text('Player List, Waiting for player'),
+      child: Text(
+        'Waiting for players to join..',
+        style: TextStyle(
+          color: Colors.grey[200],
+          fontSize: 16,
+          fontStyle: FontStyle.italic,
+          fontFamily: "Raleway",
+        ),
+      ),
     );
   }
 }
