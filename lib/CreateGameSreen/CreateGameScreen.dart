21365c121ff0e71ed1bf138ee9012d7b809e3043
Lopez <hansel.lopez@windows.nagrastar.com>
2019-01-05 17:06:59 -0700

    update: Lint widgets to screen

--- lib/CreateGameSreen/CreateGameScreen.dart
+++ lib/CreateGameSreen/CreateGameScreen.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/CreateGameSreen/GameOptions.dart';
+import 'package:hello_world/CreateGameSreen/InviteWidget.dart';
+import 'package:hello_world/CreateGameSreen/PlayerList.dart';
 import 'package:hello_world/Shared/Navigate.dart';
 import 'package:hello_world/Shared/SpyfallPage.dart';
 import 'package:hello_world/Shared/ActionBar.dart';
 import 'package:hello_world/Shared/TopHeader.dart';
-import 'package:hello_world/Shared/BorderContainer.dart';
 
 class CreateGameScreen extends StatelessWidget {
   final String title = "Create A New Game";
+  final String accessCode = "abc123";
 
   @override
   Widget build(BuildContext context) {
     Navigate navigate = Navigate.of(context);
+    Function startGame = () => navigate.goToPage(Pages.ONGOING_GAME_SCREEN);
+    Function goHomeScreen = () => navigate.goToPage(Pages.HOME_SCREEN);
 
     final List<MenuItem> menuItems = [
-      MenuItem("Create", () => navigate.goToPage(Pages.PRE_GAME_SCREEN)),
-      MenuItem("Go Back", () => navigate.goToPage(Pages.HOME_SCREEN)),
+      MenuItem("Start Game", startGame),
+      MenuItem("Cancel", goHomeScreen),
     ];
 
     return SpyfallPage(
       child: Column(
         children: <Widget>[
           TopHeader(title),
-          BorderContainer(
-            child: ActionBar(menuItems),
-          ),
+          InviteWidget(accessCode: accessCode),
+          PlayerList(),
+          GameOptions(menuItems: menuItems),
         ],
       ),
     );
